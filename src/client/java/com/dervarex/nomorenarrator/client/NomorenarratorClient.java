d59ddc1e04c7a9e81da7cff18a865caa6f9b49b8
dervarex <177766112+dervarex@users.noreply.github.com>
2026-09-24 18:24:03 +0200

    bump version to 26.1 and update mappings to mojmaps

--- src/client/java/com/dervarex/nomorenarrator/client/NomorenarratorClient.java
+++ src/client/java/com/dervarex/nomorenarrator/client/NomorenarratorClient.java
@@ -2,24 +2,20 @@ package com.dervarex.nomorenarrator.client;
 
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
-import net.minecraft.client.option.NarratorMode;
-import net.minecraft.client.util.InputUtil;
-import org.lwjgl.glfw.GLFW;
+import net.minecraft.client.NarratorStatus;
 
 public class NomorenarratorClient implements ClientModInitializer {
-    private boolean narratorShortcutHeld;
-
     @Override
     public void onInitializeClient() {
         ClientTickEvents.END_CLIENT_TICK.register(client -> {
             // Narrator off
-            if (client.options.getNarrator().getValue() != NarratorMode.OFF) {
-                client.options.getNarrator().setValue(NarratorMode.OFF);
+            if (client.options.narrator().get() != NarratorStatus.OFF) {
+                client.options.narrator().set(NarratorStatus.OFF);
             }
 
             // deactivate hotkey
-            if (client.options.getNarratorHotkey().getValue()) {
-                client.options.getNarratorHotkey().setValue(false);
+            if (client.options.narratorHotkey().get()) {
+                client.options.narratorHotkey().set(false);
             }
         });
     }
--- src/client/java/com/dervarex/nomorenarrator/mixin/client/NarratorManagerMixin.java
+++ src/client/java/com/dervarex/nomorenarrator/mixin/client/NarratorManagerMixin.java
@@ -1,17 +1,16 @@
 package com.dervarex.nomorenarrator.mixin.client;
 
-import net.minecraft.client.util.NarratorManager;
+import com.mojang.text2speech.Narrator;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(NarratorManager.class)
-public class NarratorManagerMixin {
+@Mixin(Narrator.class)
+public interface NarratorManagerMixin {
 
-    @Inject(method = "isActive", at = @At("HEAD"), cancellable = true)
+    @Inject(method = "active", at = @At("HEAD"), cancellable = true)
     private void disableNarrator(CallbackInfoReturnable<Boolean> cir) {
         cir.setReturnValue(false);
     }
 }
-
